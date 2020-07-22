33c04bb0273dfc0a40ba72286489550a85d36f27
vctt94 <victor.duarte.oliveira@gmail.com>
2020-07-22 17:56:35 -0300

    Return hex seed when entering xPrivKey

---
+++ RecoverWebWallet.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"encoding/hex"
+	"fmt"
+	"log"
+
+	"github.com/btcsuite/btcutil/hdkeychain"
+)
+
+func main() {
+	var xPrivKey string
+	fmt.Print("Enter text: ")
+	fmt.Scanln(&xPrivKey)
+	key, err := hdkeychain.NewKeyFromString(xPrivKey)
+
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+
+	if key.Depth() != 0 {
+		log.Fatal("This xPrivKey is not a seed")
+		return
+	}
+	if key.ParentFingerprint() != 0 {
+		log.Fatal("This xPrivKey is not a seed")
+		return
+	}
+
+	privKey, err := key.ECPrivKey()
+	fmt.Printf("Hex seed: %s\n", hex.EncodeToString(privKey.Serialize()))
+}
---
+++ go.mod
@@ -0,0 +1,7 @@
+module github.com/vctt94/recover-web-wallet
+
+go 1.14
+
+require (
+	github.com/btcsuite/btcutil v1.0.2
+)
